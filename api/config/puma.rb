c6b2e828d6ffd0736d456682471168f1c83792b5
watthajak2018 <watthajak@abroaders.jp>
2020-01-21 08:55:41 +0700

    【API】Added the Puma SSL setting.

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -10,7 +10,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch('PORT') { 3000 }
+# port        ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -36,3 +36,10 @@ pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if ENV.fetch('RAILS_ENV') { 'development' } == 'development'
+  ssl_bind '0.0.0.0', '3000',
+           key: '/home/root/.ssh/server.key',
+           cert: '/home/root/.ssh/server.crt'
+  worker_timeout 500
+end

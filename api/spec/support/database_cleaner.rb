8e13b3a7db338d8a35ad50b30ca15da2c339a548
watthajak2018 <watthajak@abroaders.jp>
2020-01-25 23:24:16 +0700

    【API】Installed the RSpec.
    docker-compose exec api sh
    ▶ rails g rspec:install

---
+++ api/spec/support/database_cleaner.rb
@@ -0,0 +1,14 @@
+RSpec.configure do |config|
+  config.before :suite do
+    DatabaseCleaner.clean_with :truncation
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+end

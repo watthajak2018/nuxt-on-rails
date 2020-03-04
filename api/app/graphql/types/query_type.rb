e5c28685c1049f21a978d5596bade5210fd1e16d
watthajak2018 <watthajak@abroaders.jp>
2020-03-04 15:45:55 +0700

    【API - BLOG】Added the show blog post (GraphQL queries).

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -3,6 +3,12 @@ module Types
     field :blogposts, resolver: Resolvers::Blogposts
     field :user_information, resolver: Queries::UserInformation
 
+    field :blogpost, Types::BlogpostType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def blogpost(id:)
+      Blogpost.find id
     end
   end
 end
